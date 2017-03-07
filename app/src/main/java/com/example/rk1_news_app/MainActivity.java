6fb2c2810d78d90f91de45fd008d0238755b6ac3
Victor Kamyshin <Kamyshin.Vcitor@yandex.ru>
2017-03-07 19:18:17 +0300

    recieve news

--- app/src/main/java/com/example/rk1_news_app/MainActivity.java
+++ app/src/main/java/com/example/rk1_news_app/MainActivity.java
@@ -1,41 +1,82 @@
 package com.example.rk1_news_app;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import ru.mail.weather.lib.News;
 import ru.mail.weather.lib.Storage;
-import ru.mail.weather.lib.Topics;
-import ru.mail.weather.lib.Scheduler;
-import ru.mail.weather.lib.NewsLoader;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String ACTION_GET_NEWS = "action.GET_NEWS";
 
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    private final static boolean backgroundUpdate = false;
+
+    private BroadcastReceiver broadcastReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btn_pick_theme).setOnClickListener(onThemeClick);
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                printNews();
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter(ACTION_GET_NEWS);
+        registerReceiver(broadcastReceiver, intentFilter);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, Storage.getInstance(this).loadCurrentTopic());
-        loadTopic();
+        printTopic();
+        loadNews();
     }
 
-    private void loadTopic(){
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+        if(broadcastReceiver != null) {
+            unregisterReceiver(broadcastReceiver);
+            broadcastReceiver = null;
+        }
+    }
+
+    private void printTopic(){
         String topic = Storage.getInstance(this).loadCurrentTopic();
         if(topic!=null) {
             ((Button) findViewById(R.id.btn_pick_theme)).setText(topic);
         }
+    }
+
+    private void printNews(){
+        News news = Storage.getInstance(this).getLastSavedNews();
+
+        ((TextView) findViewById(R.id.text_news_title)).setText(news.getTitle());
+        ((TextView) findViewById(R.id.text_news_text)).setText(news.getBody());
+       // ((TextView) findViewById(R.id.text_news_date)).setText(news.getDate());
+        printTopic();
+    }
+
+    private void loadNews(){
+        Intent intent = new Intent(MainActivity.this, NewsIntentService.class);
+        startService(intent);
     }
 
 
---
+++ app/src/main/java/com/example/rk1_news_app/NewsIntentService.java
@@ -0,0 +1,37 @@
+package com.example.rk1_news_app;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+
+import ru.mail.weather.lib.News;
+import ru.mail.weather.lib.NewsLoader;
+import ru.mail.weather.lib.Storage;
+
+
+public class NewsIntentService extends IntentService {
+
+    public NewsIntentService() {
+        super("WeatherIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            try {
+                String currentTopic = Storage.getInstance(getApplicationContext()).loadCurrentTopic();
+                NewsLoader newsLoader =new NewsLoader();
+                News news = newsLoader.loadNews(currentTopic);
+                Storage.getInstance(getApplicationContext()).saveNews(news);
+                Intent resultIntent = new Intent(MainActivity.ACTION_GET_NEWS);
+                sendBroadcast(resultIntent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
